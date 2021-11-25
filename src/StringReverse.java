d2268cb35b0198e29292ddf787f8ec21c33cecfa
srinivasreddykollu <srinivasreddykollu1@gmail.com>
2021-11-25 20:10:10 +0530

    stringreverse

---
+++ src/StringReverse.java
@@ -0,0 +1,13 @@
+public class StringReverse {
+    String rev="";
+    public StringReverse(String str){
+        char[] ch=str.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            rev=ch[i]+rev;
+        }
+        System.out.println(rev);
+    }
+    public static void main(String[] args){
+        StringReverse obj=new StringReverse("kollu");
+    }
+}
