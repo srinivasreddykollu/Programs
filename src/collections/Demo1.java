4d2ffbed23739b3e3e7cc326a52918f41f1d0218
srinivasreddykollu <srinivasreddykollu1@gmail.com>
2021-11-27 10:55:10 +0530

    list change

--- src/collections/Demo1.java
+++ src/collections/Demo1.java
@@ -1,9 +1,6 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
+import java.util.*;
 
 public class Demo1 {
     public static void main(String[] args){
@@ -11,6 +8,14 @@ public class Demo1 {
         obj.add("kollu");
         obj.add(2);
         obj.add(45);
+        //this collection interface wont support adding objects with index so we can make as list interface and we can make
+        //it as generic with same type of objects with
+        List<Integer> obj1=new ArrayList<Integer>();
+        obj1.add(8);
+        obj1.add(56);
+        obj1.add(1,80);
+
+
      //  Iterator i= obj.iterator();
      //   while(i.hasNext()){
         //    System.out.println(i.next());
